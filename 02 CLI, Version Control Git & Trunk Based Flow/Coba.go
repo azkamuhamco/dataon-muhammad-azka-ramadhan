e1154b2e99898bdd01b9673fbdfcbdabf9e9113c
azkamuhamco <reprokbas@gmail.com>
2022-06-01 18:36:57 +0700

    Feat A rev 01

--- 02 CLI, Version Control Git & Trunk Based Flow/Coba.go
+++ 02 CLI, Version Control Git & Trunk Based Flow/Coba.go
@@ -4,5 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")		// master
-	fmt.Println("New feature A")
+<<<<<<< Updated upstream
+	fmt.Println("New feature A - new")
+=======
+	fmt.Println("New feature A - rev 01")
+>>>>>>> Stashed changes
 }
