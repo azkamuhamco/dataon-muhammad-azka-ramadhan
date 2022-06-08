fc96444f5cefb295be4a4859ac672cff6b13267b
azkamuhamco <reprokbas@gmail.com>
2022-06-08 23:24:03 +0700

    Day 7 Dynamic Problem 1-2 OK

---
+++ 07 Problem Solving Paradigm (Dynamic Programming)/Problem_3.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func Frog(arr []int) int {
+
+}
+
+func main() {
+	fmt.Println(Frog([]int{10, 30, 40, 20}))			// 20
+	fmt.Println(Frog([]int{30, 10, 60, 10, 60, 50}))	// 40
+}
