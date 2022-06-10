d716b89d0512f30a13f591d34be888dbcd092117
azkamuhamco <reprokbas@gmail.com>
2022-06-10 21:01:43 +0700

    Day 6 Recursive FIX Mantep

--- 06 Recursive, Number Theory, Sorting and Searching/Problem_8.go
+++ 06 Recursive, Number Theory, Sorting and Searching/Problem_8.go
@@ -41,9 +41,9 @@ func MostAppearItem(items []string) []pair {
 
 func main() {
 	fmt.Println(MostAppearItem([]string{"js", "js", "golang", "ruby", "ruby", "js", "js"}))
-	// golang->1 ruby->2 js-4
+	// [{golang 1} {ruby 2} {js 4}]
 	fmt.Println(MostAppearItem([]string{"A", "B", "B", "C", "A", "A", "B", "A", "D", "D"}))	
-	// C->1 D->2 B->3 A->4
+	// [{C 1} {D 2} {B 3} {A 4}]
 	fmt.Println(MostAppearItem([]string{"football", "basketball", "tenis"}))	
-	// football->1 basketball->1 tenis->1
+	// [{football 1} {basketball 1} {tenis 1}]
 }
