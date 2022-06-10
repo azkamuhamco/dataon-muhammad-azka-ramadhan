c9d657dece00751c59c45497d3750b366dbbbcac
azkamuhamco <reprokbas@gmail.com>
2022-06-10 14:45:29 +0700

    Day 6 Recursive Problem 1-7 DONE

--- 06 Recursive, Number Theory, Sorting and Searching/Problem_7.go
+++ 06 Recursive, Number Theory, Sorting and Searching/Problem_7.go
@@ -1,16 +1,25 @@
 package main
 import "fmt"
 
-func PlayingDomino(cards [][]int, deck []int) interface{} {
-	return Deck(deck)
-}
+func playingDomino(cards [][]int, deck []int) interface{} {
+	var hasil []int
 
-func Deck(deck []int) []int{
-	return deck
+    for i:=0; i<len(cards); i++ {
+		for j:=0; j<2; j++ {
+			if deck[0] == cards[i][j] || deck[1] == cards[i][j] {
+				hasil = append(hasil, cards[i]...)
+			}
+		}
+	}
+
+	if len(hasil) == 0 {
+		return "tutup kartu"
+	}
+	return hasil[0:2]
 }
 
 func main() {
-	fmt.Println([][]int{[]int{6, 5}, []int{3, 4}, []int{2, 1}, []int{3, 3}}, []int{4, 3})
-	fmt.Println([][]int{[]int{6, 5}, []int{3, 3}, []int{3, 4}, []int{2, 1}}, []int{3, 6})
-	fmt.Println([][]int{[]int{6, 6}, []int{2, 4}, []int{3, 6}}, []int{5, 1})
+	fmt.Println(playingDomino([][]int{[]int{6, 5}, []int{3, 4}, []int{2, 1}, []int{3, 3}}, []int{4, 3}))
+	fmt.Println(playingDomino([][]int{[]int{6, 5}, []int{3, 3}, []int{3, 4}, []int{2, 1}}, []int{3, 6}))
+	fmt.Println(playingDomino([][]int{[]int{6, 6}, []int{2, 4}, []int{3, 6}}, []int{5, 1}))
 }
