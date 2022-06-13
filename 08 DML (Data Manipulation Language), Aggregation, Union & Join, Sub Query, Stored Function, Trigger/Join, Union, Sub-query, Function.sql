a5ee980244cad6a66d68de37f4e9298142f07d74
azkamuhamco <reprokbas@gmail.com>
2022-06-13 14:53:10 +0700

    2022-06-13 14.53 WIB

--- 08 DML (Data Manipulation Language), Aggregation, Union & Join, Sub Query, Stored Function, Trigger/Join, Union, Sub-query, Function.sql
+++ 08 DML (Data Manipulation Language), Aggregation, Union & Join, Sub Query, Stored Function, Trigger/Join, Union, Sub-query, Function.sql
@@ -2,13 +2,13 @@
 SELECT * FROM transactions t INNER JOIN users u ON t.user_id = u.id WHERE u.id = 1 OR u.id = 2
 
 -- 2 Tampilkan jumlah harga transaksi user id 1
-
+SELECT t.total_price FROM transactions t INNER JOIN users u ON t.user_id = u.id WHERE u.id = 1
 
 -- 3 Tampilkan total transaksi dengan product type 2
-
+SELECT COUNT(td.price) FROM transaction_details td INNER JOIN products p ON td.product_id = p.id WHERE p.id = 2
 
 -- 4 Tampilkan semua field prod dan field name table product type yg saling berhub
-
+SELECT * FROM products p INNER JOIN product_types pt ON p.product_type_id = pt.id
 
 -- 5 Tampilkan semua field table transaction, field name table product dan field name table user
 
