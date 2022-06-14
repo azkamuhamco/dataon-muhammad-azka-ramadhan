d95154ece9ab96bd82d9f49abcee8fed290ab59f
azkamuhamco <reprokbas@gmail.com>
2022-06-14 08:44:15 +0700

    2022-06-14 08.44 WIB

--- 09 Mongo DB 1/3 Update.js
+++ 09 Mongo DB 1/3 Update.js
@@ -1,5 +1,5 @@
 // a. Ubah data product id 1 jadi "product dummy"
-
+db.products.updateOne( { id: 1 }, { $set: { name: 'product dummy' } } );
 
 // b. Ubah qty=3 pada transaction_details dg product id 1
-
+db.transaction_details.updateMany( { product_id: 1 }, { $set: { qty: 3 } } );
