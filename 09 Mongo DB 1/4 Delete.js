83d1f79001586299e49c540a02bba99264255a3d
azkamuhamco <reprokbas@gmail.com>
2022-06-14 08:47:01 +0700

    2022-06-14 08.46 WIB

--- 09 Mongo DB 1/4 Delete.js
+++ 09 Mongo DB 1/4 Delete.js
@@ -1,5 +1,5 @@
 // a. Delete data pada tabel product dg id=1
-
+db.products.deleteOne( { id: 1 } );
 
 // b. Delete data pada tabel product dg prod type id=1
-
+db.products.deleteOne( { product_type_id: 1 } );
