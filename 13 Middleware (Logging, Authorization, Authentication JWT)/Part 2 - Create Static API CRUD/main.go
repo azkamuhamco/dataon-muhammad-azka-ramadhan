d7ed7e0cdcb3ac5dec565235e79ab8af838456de
azkamuhamco <reprokbas@gmail.com>
2022-06-17 19:20:46 +0700

    2022-06-17 19.20 WIB

---
+++ 13 Middleware (Logging, Authorization, Authentication JWT)/Part 2 - Create Static API CRUD/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"net/http"
+	// "strconv"
+	"github.com/labstack/echo"
+)
+
+type User struct {
+	Id       int    `json:"id" form:"id"`
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+}
+
+var users []User
+
+// -------- controller --------
+
+// get all users
+func GetUsersController(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"messages": "success get all users",
+		"users":    users,
+	})
+}
+
+// // get user by id
+// func GetUserController(c echo.Context) error {
+// 	// solution here
+// }
+
+// // delete user by id
+// func DeleteUserController(c echo.Context) error {
+// 	// solution here
+// }
+
+// // update user by id
+// func UpdateUserController(c echo.Context) error {
+// 	// solution here
+// }
+
+// create new user
+func CreateUserController(c echo.Context) error {
+	// binding data
+	user := User{}
+	c.Bind(&user)
+
+	if len(users) == 0 {
+		user.Id = 1
+	} else {
+		newId := users[len(users)-1].Id + 1
+		user.Id = newId
+	}
+	users = append(users, user)
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"messages": "success get all users",
+		"user":     user,
+	})
+}
+
+func main() {
+	e := echo.New()
+	// routing with query param
+	e.GET("/users", GetUsersController)
+	e.POST("/users", CreateUserController)
+
+	// start the server, and log if it fails
+	e.Logger.Fatal(e.Start(":8080"))
+}
